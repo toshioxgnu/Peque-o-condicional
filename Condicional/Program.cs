7b1c870657f33cb11a5ce5d1025f675e5f8b89e4
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2020-04-05 14:53:49 -0400

    oli

---
+++ Condicional/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("Ingrese su edad para poder ingresar a la pagina malula");
+            edad = Console.Read();
+            var edadint = Convert.ToInt32(edad);
+
+            if (edadint < 18)
+            {
+                Console.WriteLine("Eres muy pequeño prueba de nuevo ");
+                edad = Console.Read();
+                edadint = Convert.ToInt32(edad);
+            }
+            else
+            {
+                Console.WriteLine("Asi me gusta una persona de avanzada edad ");
+            }
+        }
+    }
+}
